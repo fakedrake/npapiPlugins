42f2b7e5b82eb77cafebb2c0c0f78e863506c931
Konstantina Kastanara <kastanarakonstantina@gmail.com>
2014-04-04 18:48:53 +0300

    Error handling in openPort function

--- Codebendercc/CodebenderccAPI.cpp
+++ Codebendercc/CodebenderccAPI.cpp
@@ -38,17 +38,39 @@ bool CodebenderccAPI::openPort(const std::string &port, const unsigned int &baud
 					serialPort.setPort(device);				//set port name
 					serialPort.setBaudrate(baudrate);		//set port baudrate
 					serialPort.setTimeout(portTimeout);		//set the read/write timeout of the port
-					
 					serialPort.open();			//open the port
 					serialPort.setDTR(true);	//set Data Transfer signal, needed for Arduino leonardo
 					serialPort.setRTS(false);	//set Request to Send signal to false, needed for Arduino leonardo  
 				}
-			}catch(...){
-			CodebenderccAPI::debugMessage("CodebenderccAPI::openPort exception",2);
-			perror("Error opening port.");
-			return false;
-						}
-							}	
+			}catch(serial::PortNotOpenedException& pno){
+				CodebenderccAPI::debugMessage("CodebenderccAPI::PortNotOpenedException exception",2);
+				m_host->htmlLog (pno.what());
+				perror("Error opening port.");
+				RemovePortFromList(usedPort);
+				return false;
+											}
+			catch(serial::SerialException& se){
+				CodebenderccAPI::debugMessage("CodebenderccAPI::SerialException exception",2);
+				m_host->htmlLog (se.what());
+				perror("Error opening port.");
+				RemovePortFromList(usedPort);
+				return false;
+									}			
+			catch(std::invalid_argument& inv_arg){
+				CodebenderccAPI::debugMessage("CodebenderccAPI::invalid_argument exception",2);
+				m_host->htmlLog (inv_arg.what());
+				perror("Error opening port.");
+				RemovePortFromList(usedPort);
+				return false;
+									}	
+			catch(serial::IOException& IOe){
+				CodebenderccAPI::debugMessage("CodebenderccAPI::IOException exception",2);
+				m_host->htmlLog (IOe.what());
+				perror("Error opening port.");
+				RemovePortFromList(usedPort);
+				return false;
+								}						
+		}	
 	else{
 		CodebenderccAPI::debugMessage("CodebenderccAPI::Port is already in use.",3);
 		return false;
