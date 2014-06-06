6b3dbc38d2a42f526738fc2f2f2ce5b650b6b01b
Konstantina Kastanara <kastanarakonstantina@gmail.com>
2014-06-06 12:07:03 +0300

    Update RegCloseKey() / RegOpenKeyEx() error codes.
    Update RegCloseKey() error code 6 to appear as PLUGIN_WARNING
    Update RegOpenKeyEx() error code 2 to appear as PLUGIN_WARNING

--- Codebendercc/CodebenderccAPI.cpp
+++ Codebendercc/CodebenderccAPI.cpp
@@ -1527,7 +1527,10 @@ CodebenderccAPI::RegOpenKeyEx(HKEY hKey,
 		std::string err_msg = "CodebenderccAPI::RegOpenKeyEx() - Winerror.h error code: ";
 		err_msg += boost::lexical_cast<std::string>(rc);
 
-		error_notify(err_msg);
+		if(rc==2)
+			error_notify(err_msg,1);
+		else
+			error_notify(err_msg);
 		return rc;
 	}
 	
@@ -1555,7 +1558,10 @@ CodebenderccAPI::RegCloseKey(HKEY hKey)
 		std::string err_msg = "CodebenderccAPI::RegCloseKey() - Winerror.h error code: ";
 		err_msg += boost::lexical_cast<std::string>(rc);
 
-		error_notify(err_msg);
+		if(rc==6)
+			error_notify(err_msg,1);
+		else
+			error_notify(err_msg);
 		return rc;
 	}
 
