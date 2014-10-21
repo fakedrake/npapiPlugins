a26ae880ac2482a995ae0ef3ac4ebae5a35f2703
Chris Perivolaropoulos <darksaga2006@gmail.com>
2014-10-21 16:35:23 +0300

    Linux build complete with doc

--- build.sh
+++ build.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+set -e
 
-firebreath-dev/prepmake.sh Codebendercc build -D CMAKE_BUILD_TYPE="Debug"
+git submodule init
+git submodule update
+firebreath-dev/prepmake.sh Codebendercc build-linux -D CMAKE_BUILD_TYPE="Debug"
 make -C build
+
+ffdir=$HOME/.mozilla/firefox/*.default/extensions/codebender@codebender.cc/plugins
+so=$(pwd)/build-linux/bin/Codebendercc/npCodebendercc.so
+
+echo "###########################################################################"
+echo "Your plugin .so is in $(pwd)/build-linux/bin/Codebendercc/npCodebendercc.so"
+echo "firefox:"
+echo "    mkdir -p $ffdir"
+echo "    ln -s $so $ffdir"
+echo "###########################################################################"
