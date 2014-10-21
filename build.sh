ca387a9033fa8b3a24308971ec59655298a6f872
Chris Perivolaropoulos <darksaga2006@gmail.com>
2014-10-21 16:40:15 +0300

    Fixed paths

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -e
 
+build_dir=build-linux
+
 git submodule init
 git submodule update
-firebreath-dev/prepmake.sh Codebendercc build-linux -D CMAKE_BUILD_TYPE="Debug"
-make -C build
+firebreath-dev/prepmake.sh Codebendercc $build_dir -D CMAKE_BUILD_TYPE="Debug"
+make -C $build_dir
 
 ffdir=$HOME/.mozilla/firefox/*.default/extensions/codebender@codebender.cc/plugins
-so=$(pwd)/build-linux/bin/Codebendercc/npCodebendercc.so
+so=$(pwd)/$build_dir/bin/Codebendercc/npCodebendercc.so
 
 echo "###########################################################################"
 echo "Your plugin .so is in $(pwd)/build-linux/bin/Codebendercc/npCodebendercc.so"
